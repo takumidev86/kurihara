c264bca92f224622e68b8752fb56a75cbdc859a1
Takumi Kurihara <takumidevelopment86@gmail.com>
2021-05-16 17:44:12 +0900

    ♻️ : Create headline

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Links } from "../components/Links";
+import { Headline } from "../components/Headline";
 
 export default function Home() {
   return (
@@ -13,12 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Blog</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/blogs.js</code>
-        </p>
+        <Headline page="blog"></Headline>
         <Links></Links>
       </main>
       <Footer></Footer>
