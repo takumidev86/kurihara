603dcc53ec25de6f657e200792ffe2b97c22dd00
Takumi Kurihara <takumidevelopment86@gmail.com>
2021-08-14 17:25:03 +0900

    Header Tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,15 @@
-import styles from "./Header.module.css";
-// import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
     // <header className={styles.header}>
     <header className="border-b-2 md:border-t-2 text-center container mx-auto ">
-      {/* <Link></Link>
-      <a href="/">index</a>
-      <a href="blogs">blogs</a> */}
       <Link href="/">
-        <a className={styles.anchor}>Index</a>
+        <a className="inline-block py-1 px-6 text-xl">Index</a>
       </Link>
+
       <Link href="/blogs">
-        <a className={styles.anchor}>Blog</a>
+        <a className="inline-block py-1 px-6 text-xl">Blog</a>
       </Link>
     </header>
   );
