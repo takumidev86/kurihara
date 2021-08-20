40ef46b569ebdc0c02a204e8fb0a0efffa4d7083
Takumi Kurihara <takumidevelopment86@gmail.com>
2021-08-21 02:51:49 +0900

    Create HamburgerMenu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,22 @@
 import Link from "next/link";
 import { VFC } from "react";
-import { Flex, Heading, Box, Text, Spacer } from "@chakra-ui/react";
-
+import {
+  Flex,
+  Heading,
+  Box,
+  Text,
+  Spacer,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 export const Header: VFC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Flex
@@ -15,7 +29,7 @@ export const Header: VFC = () => {
         <Flex align="center" as="a" mr={10} _hover={{ cursor: "pointer" }}>
           <Text fontSize={{ base: "md", md: "xl" }} fontFamily="mono">
             <Link href="/">
-              <Text color="blue.500"> kurihara.dev</Text>
+              <Text color="blue.600"> kurihara.dev</Text>
             </Link>
           </Text>
         </Flex>
@@ -25,43 +39,131 @@ export const Header: VFC = () => {
           flexGrow={2}
           display={{ base: "none", md: "flex" }}
         >
-          <Box
-            p={2}
-            _hover={{
-              cursor: "pointer",
-              bg: "gray.100",
-              borderRadius: "5px",
-              color: "blue.500",
-            }}
-          >
-            <Link href="/">Home</Link>
-          </Box>
-          <Box
-            as="a"
-            href="/blogs"
-            p={2}
-            _hover={{
-              cursor: "pointer",
-              bg: "gray.100",
-              borderRadius: "5px",
-              color: "blue.500",
-            }}
-          >
-            Blog
-          </Box>
-          <Box
-            p={2}
-            _hover={{
-              cursor: "pointer",
-              bg: "gray.100",
-              borderRadius: "5px",
-              color: "blue.500",
-            }}
-          >
-            <Link href="/profile"> Profile</Link>
-          </Box>
+          <Link href="/">
+            <Box
+              p={2}
+              _hover={{
+                cursor: "pointer",
+                bg: "gray.100",
+                borderRadius: "5px",
+                color: "blue.500",
+              }}
+            >
+              Home
+            </Box>
+          </Link>
+          <Link href="/blogs">
+            <Box
+              p={2}
+              _hover={{
+                cursor: "pointer",
+                bg: "gray.100",
+                borderRadius: "5px",
+                color: "blue.500",
+              }}
+            >
+              Blog
+            </Box>
+          </Link>
+          <Link href="/profile">
+            <Box
+              p={2}
+              _hover={{
+                cursor: "pointer",
+                bg: "gray.100",
+                borderRadius: "5px",
+                color: "blue.500",
+              }}
+            >
+              Profile
+            </Box>
+          </Link>
         </Flex>
+        <IconButton
+          aria-label="memu-button"
+          icon={<HamburgerIcon />}
+          size="lg"
+          variant="unstyled"
+          display={{ base: "block", md: "none" }}
+          onClick={onOpen}
+        ></IconButton>
       </Flex>
+      <Drawer placement="bottom" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              {/* <Link href="/">
+                <Button
+                  w="95%"
+                  m="10px"
+                  colorScheme="blackAlpha"
+                  variant="ghost"
+                >
+                  Home
+                </Button>
+              </Link>
+              <Link href="/blogs">
+                <Button w="95%" m="10px" colorScheme="blue" variant="ghost">
+                  Blogs
+                </Button>
+              </Link>
+              <Link href="/profile">
+                <Button w="95%" m="10px" colorScheme="blue" variant="ghost">
+                  Profile
+                </Button>
+              </Link> */}
+              <Link href="/">
+                <Box
+                  p={2}
+                  w="95%"
+                  m="10px"
+                  textAlign="center"
+                  _hover={{
+                    cursor: "pointer",
+                    bg: "gray.300",
+                    borderRadius: "5px",
+                    color: "blue.500",
+                  }}
+                >
+                  Home
+                </Box>
+              </Link>
+              <Link href="/blogs">
+                <Box
+                  p={2}
+                  w="95%"
+                  m="10px"
+                  textAlign="center"
+                  _hover={{
+                    cursor: "pointer",
+                    bg: "gray.300",
+                    borderRadius: "5px",
+                    color: "blue.500",
+                  }}
+                >
+                  Blog
+                </Box>
+              </Link>
+              <Link href="/profile">
+                <Box
+                  p={2}
+                  w="95%"
+                  m="10px"
+                  textAlign="center"
+                  _hover={{
+                    cursor: "pointer",
+                    bg: "gray.300",
+                    borderRadius: "5px",
+                    color: "blue.500",
+                  }}
+                >
+                  Profile
+                </Box>
+              </Link>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </>
   );
 };
